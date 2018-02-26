0def31c6a1a956239353268e7e0cb2da8d1bb277
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-02-26 18:57:47 +0000

    Firebase setup

---
+++ app/config/index.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyACpPU_oR053j54nxff4HTQVgah7VUSSX4",
+  authDomain: "oderit-store.firebaseapp.com",
+  databaseURL: "https://oderit-store.firebaseio.com",
+  projectId: "oderit-store",
+  storageBucket: "oderit-store.appspot.com",
+  messagingSenderId: "962177871078"
+}
